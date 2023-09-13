f70dc65a5d717e387bf7552372f46733edc61840
Naman Yadav <raymannky2001@gmail.com>
2023-09-13 19:35:22 +0530

    setuping the project starter files then added react router and setup main pages working on menu page

---
+++ src/ui/Header.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">PizzaPal & Co.</Link>
+    </header>
+  );
+}
+
+export default Header;
