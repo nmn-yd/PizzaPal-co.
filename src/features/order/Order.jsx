e1b78e8c40f1cbabb356fb79dd475b459425c77f
Naman Yadav <raymannky2001@gmail.com>
2023-09-14 01:23:59 +0530

    fetching data using react router form and sending cart and deleviry data using react router action done

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,47 +1,15 @@
 // Test ID: IIDSAT
 
+import { useLoaderData } from "react-router-dom";
+import { getOrder } from "../../services/apiRestaurant";
 import {
   calcMinutesLeft,
   formatCurrency,
   formatDate,
 } from "../../utils/helpers";
 
-const order = {
-  id: "ABCDEF",
-  customer: "Jonas",
-  phone: "123456789",
-  address: "Arroios, Lisbon , Portugal",
-  priority: true,
-  estimatedDelivery: "2027-04-25T10:00:00",
-  cart: [
-    {
-      pizzaId: 7,
-      name: "Napoli",
-      quantity: 3,
-      unitPrice: 16,
-      totalPrice: 48,
-    },
-    {
-      pizzaId: 5,
-      name: "Diavola",
-      quantity: 2,
-      unitPrice: 16,
-      totalPrice: 32,
-    },
-    {
-      pizzaId: 3,
-      name: "Romana",
-      quantity: 1,
-      unitPrice: 15,
-      totalPrice: 15,
-    },
-  ],
-  position: "-9.000,38.000",
-  orderPrice: 95,
-  priorityPrice: 19,
-};
-
 function Order() {
+  const order = useLoaderData();
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
     id,
@@ -81,6 +49,14 @@ function Order() {
       </div>
     </div>
   );
+}
+
+/*
+now below we need to useParam to get the id from url and then send it into the getorder but that useParam only works inside the component since its a hook so we cannot use in regular fucntion but react router thought of this already therefore it passes in some data into the loader function as it calls it, and one of the properties of the object that the loader function receives is exactly the params.*/
+
+export async function loader({ params }) {
+  const order = await getOrder(params.orderId);
+  return order;
 }
 
 export default Order;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/searchOrder";
 
 function Header() {
   return (
     <header>
       <Link to="/">PizzaPal & Co.</Link>
+      <SearchOrder />
     </header>
   );
 }
